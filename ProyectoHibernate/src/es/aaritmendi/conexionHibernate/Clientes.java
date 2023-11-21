50bcc9da034fa3bc1cae4d605a10163761ba52da
aaritmendi <65676135+aaritmendi@users.noreply.github.com>
2023-11-21 17:55:01 +0100

    Primeros pasos con BBDD
    49

---
+++ ProyectoHibernate/src/es/aaritmendi/conexionHibernate/Clientes.java
@@ -0,0 +1,74 @@
+package es.aaritmendi.conexionHibernate;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="clientes")
+public class Clientes {
+		
+	
+	public Clientes(String nombre, String apellidos, String direccion) {
+		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.direccion = direccion;
+	}
+
+	public Clientes() {
+	}
+	
+	@Override
+	public String toString() {
+		return "Clientes [id=" + id + ", nombre=" + nombre + ", apellidos=" + apellidos + ", direccion=" + direccion
+				+ "]";
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+
+
+	@Id
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="Nombre")
+	private String nombre;
+	
+	@Column(name="Apellidos")
+	private String apellidos;
+	
+	@Column(name="Direccion")
+	private String direccion;
+	
+	
+
+}
---
+++ ProyectoHibernate/src/es/aaritmendi/conexionHibernate/GuardaClientePrueba.java
@@ -0,0 +1,35 @@
+package es.aaritmendi.conexionHibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class GuardaClientePrueba {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Crear un objeto sesion factory
+		SessionFactory miFactory=new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Clientes.class).buildSessionFactory();
+		
+		//Crear el objeto de tipo session
+		Session miSession=miFactory.openSession();	
+		
+		try {
+			Clientes cliente1=new Clientes("Manolo","Diaz","Lugo");
+			miSession.beginTransaction();
+			miSession.save(cliente1);
+			miSession.getTransaction().commit();
+			System.out.println("Registro insertado con exito");
+			miSession.close();
+			
+		} finally{
+			miFactory.close();
+		}
+		
+		
+		
+
+	}
+
+}
